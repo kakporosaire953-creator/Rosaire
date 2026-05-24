aa272518534f38df9ed78e936bb549432d306f2e
Rosaire Dev <dev@rosaire.com>
2026-05-24 14:34:42 +0200

    Fix: Remove pino-http dependency and use simple logging middleware

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,17 +1,18 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import * as pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
-const pinoHttp = pinoHttpModule.default || pinoHttpModule;
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-  }),
-);
+// Simple request logging middleware
+app.use((req, res, next) => {
+  logger.info({
+    method: req.method,
+    url: req.url?.split("?")[0],
+  });
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
